500644b3e781728880e5f37d80b038358686c80c
Neils Nesse <nnesse@nthdimtech.com>
2018-05-09 21:46:52 -0700

    WIndows fixes to raw HID client

--- client/raw_hid_msg_win32.c
+++ client/raw_hid_msg_win32.c
@@ -6,19 +6,18 @@
 
 int main(int argc, char **argv)
 {
-	char hid_buffer[64 + 1];
-	if (argc < 3) {
-		fprintf(stderr, "Usage %s <device file> <message>\n", argv[0]);
+	char hid_buffer[64];
+	if (argc < 2) {
+		fprintf(stderr, "Usage %s <message>\n", argv[0]);
 		return -1;
 	}
 	int count = rawhid_open(1, 0x5E2A /* VID */ , 1 /* PID */, 0xffab /* usage page */, 0x200 /* USAGE */);
 	if (count != 1) {
 		fprintf(stderr, "Couldn't open device\n");
-	
-	hid_buffer[0] = 0;
-	if (strlen(argv[2]) > 63)
+	}
+	if (strlen(argv[1]) > 63)
 		argv[63] = 0;
-	strcpy(hid_buffer + 1, argv[2]);
+	strcpy(hid_buffer, argv[1]);
 	
 	rawhid_send(0, hid_buffer, sizeof(hid_buffer), 1000);
 
